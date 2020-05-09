d5f92d15b103ab3eb3490387c46c4fdb94e95ce7
ARM32 <arm32.x@gmail.com>
2020-05-08 22:48:56 -0400

    Implement different sprinting modes

--- src/main/java/arm32x/minecraft/retrofoodsystem/mixin/HungerManagerMixin.java
+++ src/main/java/arm32x/minecraft/retrofoodsystem/mixin/HungerManagerMixin.java
@@ -1,14 +1,24 @@
 package arm32x.minecraft.retrofoodsystem.mixin;
 
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.gui.FabricGuiEntry;
+import net.fabricmc.loader.launch.common.FabricLauncher;
+import net.minecraft.MinecraftVersion;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.HungerManager;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import arm32x.minecraft.retrofoodsystem.config.ModConfig;
 
 @Mixin(HungerManager.class)
 public class HungerManagerMixin {
@@ -17,7 +27,9 @@ public class HungerManagerMixin {
 	
 	@Inject(method = "update(Lnet/minecraft/entity/player/PlayerEntity;)V", at = @At("HEAD"), cancellable = true)
 	public void onUpdate(PlayerEntity player, CallbackInfo ci) {
-		this.player = player;
+		if (player != null) {
+			this.player = player;
+		}
 		ci.cancel();
 	}
 	
@@ -27,6 +39,24 @@ public class HungerManagerMixin {
 			player.heal(food);
 		}
 		ci.cancel();
+	}
+	
+	@Inject(method = "getFoodLevel()I", at = @At("HEAD"), cancellable = true)
+	public void onGetFoodLevel(CallbackInfoReturnable<Integer> cir) {
+		ModConfig.SprintingMode sprintingMode = AutoConfig.getConfigHolder(ModConfig.class).getConfig().sprintingMode;
+		switch (sprintingMode) {
+			case NEVER:
+				cir.setReturnValue(0);
+				break;
+			case ABOVE_THREE_HEARTS:
+				if (player != null) {
+					cir.setReturnValue(MathHelper.clamp((int)player.getHealth(), 0, 20));
+				}
+				break;
+			case ALWAYS:
+				cir.setReturnValue(20);
+				break;
+		}
 	}
 	
 }
